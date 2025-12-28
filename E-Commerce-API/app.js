4a06e3dd71c1e6bb65f4f46ef364a0d04210de13
SalehOmar-Y <saleh.omar.uk.com>
2025-12-28 20:52:43 +0000

    Add jest and supertest for API testing

--- E-Commerce-API/app.js
+++ E-Commerce-API/app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
+
+app.get("/products", async (req, res) => {
+  // Your code to fetch products from the database should go here
+  
 // Your code to run the server should go here
 // Don't hardcode your DB password in the code or upload it to GitHub! Never ever do this ever.
 // Use environment variables instead:
