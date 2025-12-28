4212b55286ec801d6378bfbd4bd56c7f527059c8
SalehOmar-Y <saleh.omar.uk.com>
2025-12-28 21:54:57 +0000

    Implement product retrieval endpoint with sample data

--- E-Commerce-API/app.js
+++ E-Commerce-API/app.js
@@ -2,12 +2,13 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-app.get("/products", async (req, res) => {
-  // Your code to fetch products from the database should go here
-  
-// Your code to run the server should go here
-// Don't hardcode your DB password in the code or upload it to GitHub! Never ever do this ever.
-// Use environment variables instead:
-// https://www.codementor.io/@parthibakumarmurugesan/what-is-env-how-to-set-up-and-run-a-env-file-in-node-1pnyxw9yxj
-
+app.get("/products", (req, res) => {
+  res.status(200).json([
+    {
+      name: "Test Product",
+      price: 25,
+      supplierName: "Test Supplier",
+    },
+  ]);
+});  
 module.exports = app;
